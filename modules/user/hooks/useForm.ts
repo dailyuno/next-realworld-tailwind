ef39d3fd7add79a78b4913724bfa21c95df18577
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-03 16:16:40 +0900

    refactor: 반환 타입 명시

--- modules/user/hooks/useForm.ts
+++ modules/user/hooks/useForm.ts
@@ -1,6 +1,11 @@
 import { ChangeEvent, ChangeEventHandler, useCallback, useState } from "react";
 
-function useForm<T>(initialData: T) {
+type Form<T> = {
+  form: T;
+  handleInputChange: ChangeEventHandler<Element>;
+};
+
+function useForm<T>(initialData: T): Form<T> {
   const [form, setForm] = useState<T>(initialData);
   const handleInputChange: ChangeEventHandler = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
