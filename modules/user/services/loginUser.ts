686f54ec59899eda6b6d7683d9cef6b4943392a7
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-03 10:46:35 +0900

    feat: 로그인 API 추가

---
+++ modules/user/services/loginUser.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosResponse } from "axios";
+import { API_BASE_URL } from "~/common/utils/constants";
+import { UserLoginForm } from "../types/userForm";
+
+export function loginUser({
+  email,
+  password,
+}: UserLoginForm): Promise<AxiosResponse> {
+  return axios.post(`${API_BASE_URL}/users`, {
+    user: {
+      email,
+      password,
+    },
+  });
+}
