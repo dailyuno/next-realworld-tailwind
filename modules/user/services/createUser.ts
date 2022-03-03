b27a0a76a92d39fc90805969a7fee0cc2e66d852
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-03 10:45:15 +0900

    refactor: 유저 폼 타입 파일 분리

--- modules/user/services/createUser.ts
+++ modules/user/services/createUser.ts
@@ -1,11 +1,12 @@
 import axios, { AxiosResponse } from "axios";
 import { API_BASE_URL } from "~/common/utils/constants";
+import { UserRegisterForm } from "../types/userForm";
 
-export function createUser(
-  username: string,
-  email: string,
-  password: string
-): Promise<AxiosResponse> {
+export function createUser({
+  username,
+  email,
+  password,
+}: UserRegisterForm): Promise<AxiosResponse> {
   return axios.post(`${API_BASE_URL}/users`, {
     user: {
       username,
---
+++ modules/user/types/userForm.ts
@@ -0,0 +1,10 @@
+export type UserLoginForm = {
+  email: string;
+  password: string;
+};
+
+export type UserRegisterForm = {
+  username: string;
+  email: string;
+  password: string;
+};
