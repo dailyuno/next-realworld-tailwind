43ca4cded8368ffe403ace278dfd56331fc5769d
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-03 16:12:01 +0900

    refactor: 파일 분리

---
+++ modules/user/utils/setUser.ts
@@ -0,0 +1,13 @@
+import Router from "next/router";
+import { mutate } from "swr";
+import { UserResponseData } from "../types/userResponse";
+
+function setUser(data: UserResponseData): void {
+  if (data?.user) {
+    window.localStorage.setItem("user", JSON.stringify(data.user));
+    mutate("user", data.user);
+    Router.push("/");
+  }
+}
+
+export default setUser;
