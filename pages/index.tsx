8bb945466fc55e8fc4c0d53e0656e3f80783796d
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-07 01:40:03 +0900

    feat: 메인 페이지

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import PostList from "~/modules/post/components/PostList";
+import TagList from "~/modules/tag/components/TagList";
+import useUser from "~/modules/user/hooks/useUser";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const user = useUser();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,46 +18,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex">
+        <div className="w-9/12">
+          <PostList />
         </div>
-      </main>
+
+        <div className="w-3/12 px-4">
+          <TagList />
+
+          <div className="mt-4 flex justify-end">
+            {user.isLoggedIn && (
+              <Link href="/post/create">
+                <a className="px-6 py-3 rounded bg-blue-600 text-sm text-white">
+                  글작성
+                </a>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
 
       <footer className={styles.footer}>
         <a
